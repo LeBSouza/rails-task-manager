2a676a442a7ae28cddb0bc1d882e139f53b98cf0
Leandro Souza <cartaparaleandro@gmail.com>
2019-02-12 16:06:31 -0200

    done, but without refactoring and CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/list', to: 'tasks#list'
+  get '/tasks/:id', to: 'tasks#task', as: 'task'
+  get '/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
